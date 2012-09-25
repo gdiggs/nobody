b160ac3d413257030fc4ab7642c78d25a926bb0b
GordonDiggs <gordon@gordondiggs.com>
2012-09-25 00:33:31 -0400

    test inclusion in class

---
+++ test/lib/nobody/inclusion_test.rb
@@ -0,0 +1,23 @@
+require_relative '../../test_helper'
+
+describe Nobody do
+  describe 'including it in a class' do
+    it 'should make new methods' do
+      class TestClass
+        include Nobody
+        returns_true_for :true_method
+        returns_nil_for :nil_method
+        returns_false_for :false_method
+      end
+
+      TestClass.new.must_respond_to(:true_method)
+      assert TestClass.new.true_method
+
+      TestClass.new.must_respond_to(:false_method)
+      assert_equal false, TestClass.new.false_method
+
+      TestClass.new.must_respond_to(:nil_method)
+      assert_nil TestClass.new.nil_method
+    end
+  end
+end
